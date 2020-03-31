e47810d0e3d48c01271f603a94372a919370a271
Oliver Günther <mail@oliverguenther.de>
2020-03-31 15:22:46 +0200

    Rewrite component and spec to work

--- frontend/src/app/modules/grids/widgets/news/news.component.spec.ts
+++ frontend/src/app/modules/grids/widgets/news/news.component.spec.ts
@@ -1,90 +1,77 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import { WidgetNewsComponent } from './news.component';
-import { By } from '@angular/platform-browser';
-import { DebugElement, NO_ERRORS_SCHEMA, Injector } from '@angular/core';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {WidgetNewsComponent} from './news.component';
+import {DebugElement, NO_ERRORS_SCHEMA} from '@angular/core';
 //import { Promise } from 'es6-shim';
 import {NewsDmService} from "core-app/modules/hal/dm-services/news-dm.service";
-import { HttpClientModule } from '@angular/common/http';
-import { TimezoneService } from 'core-app/components/datetime/timezone.service';
-import { ConfigurationService } from 'core-app/modules/common/config/configuration.service';
-import { ConfigurationDmService } from 'core-app/modules/hal/dm-services/configuration-dm.service';
-import { UserCacheService } from 'core-app/components/user/user-cache.service';
-import { States } from 'core-app/components/states.service';
-import { UserDmService } from 'core-app/modules/hal/dm-services/user-dm.service';
-import { CollectionResource } from 'core-app/modules/hal/resources/collection-resource';
-import { NewsResource } from 'core-app/modules/hal/resources/news-resource';
-import { HalResource } from 'core-app/modules/hal/resources/hal-resource';
-import { any } from '@uirouter/core';
-import { HalResourceService } from 'core-app/modules/hal/services/hal-resource.service';
-
+import {TimezoneService} from 'core-app/components/datetime/timezone.service';
+import {ConfigurationService} from 'core-app/modules/common/config/configuration.service';
+import {States} from 'core-app/components/states.service';
+import {UserDmService} from 'core-app/modules/hal/dm-services/user-dm.service';
+import {HalResourceService} from "core-app/modules/hal/services/hal-resource.service";
+import {HttpClientModule} from "@angular/common/http";
 
 
 fdescribe('shows news', () => {
-    let app:WidgetNewsComponent;
-    let fixture:ComponentFixture<WidgetNewsComponent>;
-    let element:DebugElement;
+  let app:WidgetNewsComponent;
+  let fixture:ComponentFixture<WidgetNewsComponent>;
+  let element:DebugElement;
 
-beforeEach(() => {
+  let newsStub = {
+    id: 1,
+    title: 'Welcome to your demo project',
+    author: {
+      href: '/api/v3/users/1',
+      name: 'Foo Bar'
+    },
+    summary: 'We are glad you joined. In this module you can communicate project news to your team members.\n',
+    description: {
+      format: 'markdown',
+      raw: 'The actual news',
+      html: '<p>The actual news</p>'
+    },
+    createdAt: '2020-03-26T10:42:14Z',
+    updatedAt: '2020-03-26T10:42:14Z',
+  };
+
+  let newsDmStub = {
+    list: (_params:any) => {
+      return Promise.resolve({ elements: [newsStub] });
+    }
+  };
+
+  beforeEach(() => {
 
     TestBed.configureTestingModule({
       declarations: [
         WidgetNewsComponent
       ],
-      providers: [TimezoneService,ConfigurationService,ConfigurationDmService,
-        UserCacheService,States,UserDmService,NewsDmService,Injector,HalResourceService
+      providers: [
+        TimezoneService,
+        { provide: ConfigurationService, useValue: {} },
+        States,
+        UserDmService,
+        { provide: NewsDmService, useValue: newsDmStub },
+        HalResourceService,
       ],
-      imports:[HttpClientModule],
-      schemas:[NO_ERRORS_SCHEMA]
+      imports: [
+        HttpClientModule
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     }).compileComponents();
 
 
     fixture = TestBed.createComponent(WidgetNewsComponent);
     app = fixture.debugElement.componentInstance;
-    
   });
 
-it('should load news from the server',()=>{
-  
-  
-   let data=  [
-    {
-      _type: 'News',
-      id: 1,
-      title: 'Welcome to your demo project',
-      summary: 'We are glad you joined. In this module you can communicate project news to your team members.\n',
-      description: {
-        format: 'markdown',
-        raw: 'The actual news',
-        html: '<p>The actual news</p>'
-      },
-      createdAt: '2020-03-26T10:42:14Z',
-      updatedAt: '2020-03-26T10:42:14Z',
-      _links: {
-        self: {
-          href: '/api/v3/news/1',
-          title: 'Welcome to your demo project'
-        },
-        project: {
-          href: '/api/v3/projects/1',
-          title: 'Demo project'
-        },
-        author: {
-          href: null
-        }
-      
-    }}
-  ];
-  
-
-    app.setupNews(data);
-
-
+  it('should load news from the server', fakeAsync(() => {
+    // Detect changes to run ngOnInit
     fixture.detectChanges();
-
-  
+    // Tick to consume stubbed promise
+    tick();
 
     expect(app.entries.length).toBe(1);
-});
-
-
+  }));
 });
--- frontend/src/app/modules/grids/widgets/news/news.component.ts
+++ frontend/src/app/modules/grids/widgets/news/news.component.ts
@@ -1,12 +1,9 @@
 import {AbstractWidgetComponent} from "core-app/modules/grids/widgets/abstract-widget.component";
-import {Component, OnInit, ChangeDetectorRef, Injector, ChangeDetectionStrategy} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Injector, OnInit} from '@angular/core';
 import {I18nService} from "core-app/modules/common/i18n/i18n.service";
-import {HalResourceService} from "core-app/modules/hal/services/hal-resource.service";
 import {PathHelperService} from "core-app/modules/common/path-helper/path-helper.service";
 import {TimezoneService} from "core-components/datetime/timezone.service";
 import {NewsResource} from "core-app/modules/hal/resources/news-resource";
-import {UserCacheService} from "core-components/user/user-cache.service";
-import {UserResource} from "core-app/modules/hal/resources/user-resource";
 import {NewsDmService} from "core-app/modules/hal/dm-services/news-dm.service";
 import {CurrentProjectService} from "core-components/projects/current-project.service";
 import {DmListParameter} from "core-app/modules/hal/dm-services/dm.service.interface";
@@ -25,36 +22,24 @@ export class WidgetNewsComponent extends AbstractWidgetComponent implements OnIn
   public entries:NewsResource[] = [];
   private entriesLoaded = false;
 
-  constructor(readonly halResource:HalResourceService,
-              readonly pathHelper:PathHelperService,
+  constructor(readonly pathHelper:PathHelperService,
               readonly i18n:I18nService,
               protected readonly injector:Injector,
               readonly timezone:TimezoneService,
-              readonly userCache:UserCacheService,
               readonly newsDm:NewsDmService,
-              readonly currentProject:CurrentProjectService,
-              readonly cdr:ChangeDetectorRef) {
+              readonly currentProject:CurrentProjectService) {
     super(i18n, injector);
   }
 
   ngOnInit() {
     this.newsDm
       .list(this.newsDmParams)
-      .then((collection) => {
-        
-      var dsd=collection.elements ;
-          this.setupNews(dsd);
-      });
+      .then(collection => this.setupNews(collection.elements));
   }
 
   public setupNews(news:any[]) {
     this.entries = news;
     this.entriesLoaded = true;
-
-    const users_loaded = this.setAuthors();
-
-    Promise.all(users_loaded)
-      .then(() => this.cdr.detectChanges());
   }
 
   public get isEditable() {
@@ -62,7 +47,7 @@ export class WidgetNewsComponent extends AbstractWidgetComponent implements OnIn
   }
 
   public newsPath(news:NewsResource) {
-   // return this.pathHelper.newsPath(news.id!);
+    // return this.pathHelper.newsPath(news.id!);
   }
 
   public newsProjectPath(news:NewsResource) {
@@ -89,23 +74,11 @@ export class WidgetNewsComponent extends AbstractWidgetComponent implements OnIn
     return !this.entries.length && this.entriesLoaded;
   }
 
-  public setAuthors() {
-    return this.entries.map((entry) => {
-      if (!entry.author) {
-        return Promise.resolve();
-      }
-
-      return this.userCache
-        .require(entry.author.idFromLink)
-        .then((user:UserResource) => {
-          entry.author = user;
-        });
-    });
-  }
-
   private get newsDmParams() {
-    let params:DmListParameter = { sortBy: [['created_at', 'desc']],
-                                   pageSize: 3 };
+    let params:DmListParameter = {
+      sortBy: [['created_at', 'desc']],
+      pageSize: 3
+    };
 
     if (this.currentProject.id) {
       params['filters'] = [['project_id', '=', [this.currentProject.id]]];
